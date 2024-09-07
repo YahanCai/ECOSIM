0f915a210d0ffd99cee217b4dea5a05b0a9494ee
Yahan Cai <caiyy026@mymail.unisa.edu.au>
2024-09-07 15:50:32 +0930

    class animal

--- ecosim.py
+++ ecosim.py
@@ -6,6 +6,15 @@ class Tile(GameObject):
     def __init__(self, position, game, width=96, height=96, sourceImage=None):
         super().__init__(position, width, height, sourceImage, game)
 
+class Animal(GameObject):
+    def __init__(self, position, game, width, height, sourceImage, speed, energy):
+        super().__init__(position, width, height, sourceImage, game)
+        self.speed = speed
+        self.energy = energy
+
+    def update(self, timeElapsed):
+        pass
+
 class EcoSim(Game):
     
     def __init__(self):
